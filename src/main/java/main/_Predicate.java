187cbaab11a55648568ff39a02c45b7020d52021
guilherme95 <guilhermemello62@gmail.com>
2022-02-05 01:00:34 -0300

    predicate functionality

---
+++ src/main/java/main/_Predicate.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class _Predicate {
+    public static void main(String[] args) {
+        System.out.println(isPhoneNumberValid.test("55984098315"));
+        System.out.println(isPhoneNumberValid.and(containsNine).test("55984098315"));
+        System.out.println(isMale.test("Male", 19));
+    }
+
+    static Predicate<String> isPhoneNumberValid = phoneNumber ->
+            phoneNumber.startsWith("55") && phoneNumber.length() == 11;
+
+    static Predicate<String> containsNine = phoneNumber ->
+            phoneNumber.contains("9");
+
+    static BiPredicate<String, Integer> isMale = (gender, age) ->
+            gender.equalsIgnoreCase("male") && age >= 18;
+}
