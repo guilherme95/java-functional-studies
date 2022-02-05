a98f3014815fdbca7492b6d7cbf90347d6d6fb85
guilherme95 <guilhermemello62@gmail.com>
2022-02-05 00:22:58 -0300

    first commit with Function and BiFunction

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(incrementByOne.andThen(multiplyByTen).apply(3));
+        System.out.println(incrementByOneAndMultiplyBy.apply(3, 10));
+    }
+
+    // Function
+    static Function<Integer, Integer> incrementByOne = number->number+1;
+    static Function<Integer, Integer> multiplyByTen = number->number*10;
+
+    // BiFunction
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBy =
+            (numberToIncrementByOne, numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+}
