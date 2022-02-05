dbc84c82e1e4b0f16b8a70884b64ae638f764a32
guilherme95 <guilhermemello62@gmail.com>
2022-02-05 01:10:32 -0300

    supplier functionality

---
+++ src/main/java/main/_Supplier.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class _Supplier {
+    public static void main(String[] args) {
+        System.out.println(getJDBCUrlConnection.get());
+        System.out.println(getMultipleJDBCUrlsConnections.get());
+    }
+
+    static Supplier<String> getJDBCUrlConnection = ()->
+            "jdbc://localhost:5432/user";
+
+    static Supplier<List<String>> getMultipleJDBCUrlsConnections = () ->
+            Arrays.asList(
+                    "jdbc://localhost:5432/auth",
+                    "jdbc://localhost:5432/user",
+                    "jdbc://localhost:5432/consumer");
+}
